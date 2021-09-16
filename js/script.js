ff77d5a6962130b3fab7fbd2731fb3644e1e5859
Thibaut <herbert.thibaut@protonmail.com>
2021-09-16 17:44:49 +0200

    fetchApiZipCode

--- js/script.js
+++ js/script.js
@@ -1,9 +1,23 @@
 let searchZipCode = document.querySelector('.search-zipCode');
 let resultZipCode = document.querySelector('.result-ZipCode');
 
+let urlZipCode = 'https://geo.api.gouv.fr/communes?codePostal=';
+
+const fetchZipCode = async() => {
+    try {
+        return await fetch(`${urlZipCode + searchZipCode.value}`)
+            .then(response => response.json()
+            .then(data => console.log(data)))
+    } catch (error) {
+        console.log('erreur : ' + error);
+    }  
+};
+
 searchZipCode.addEventListener('input', () => {
     if( searchZipCode.value.length == 5) {
         resultZipCode.innerHTML = ' ' + searchZipCode.value + ' :'; 
+        let zipCode = fetchZipCode();
+        console.log(zipCode);
     }
     
 })
